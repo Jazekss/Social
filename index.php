aafa92935943ce3e990bbcdafd86973aab5a0b32
Jazeks <89478349+Jazekss@users.noreply.github.com>
2022-02-24 12:58:10 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+$title = "Soc-code";
+?>
+<head>
+	<link rel="stylesheet" href="style/css/bootstrap.css">
+  <link rel="shortcut icon" href="style/img/soc-fav.ico">
+  <title><?php echo $title; ?></title>
+</head>
+<body>
+<div class="container top">
+  <div class="row">
+    <div class="col-sm">
+      Left menu
+    </div>
+    <div class="col-sm">
+      <img class="logo-img" src="style/img/soc-logo.png">
+
+    </div>
+    <div class="col-sm">
+      Right menu
+    </div>
+  </div>
+</div>
+<!--<h3>--><?php //echo $title; ?><!--</h3>-->
+</body>
